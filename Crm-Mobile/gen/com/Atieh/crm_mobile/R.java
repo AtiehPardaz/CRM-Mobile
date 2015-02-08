aedb7db01026b081612915bd8d732a71e6bb7efc
behzadpoor <bzadpoor@gmail.com>
2015-02-08 19:20:30 +0330

    Task 38:ایجاد صفحه خانه و تقسیم آن به 9 کلید مجزا

--- Crm-Mobile/gen/com/Atieh/crm_mobile/R.java
+++ Crm-Mobile/gen/com/Atieh/crm_mobile/R.java
@@ -22,13 +22,41 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bghome=0x7f020000;
+        public static final int bglogin=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f080012;
+        public static final int action_settings=0x7f080018;
+        public static final int btn_customer=0x7f080007;
+        public static final int btn_dayview=0x7f080002;
+        public static final int btn_download=0x7f080003;
+        public static final int btn_login=0x7f080016;
+        public static final int btn_monthview=0x7f080001;
+        public static final int btn_newactivity=0x7f080005;
+        public static final int btn_newtask=0x7f080006;
+        public static final int btn_order=0x7f080008;
+        public static final int btn_upload=0x7f080004;
+        public static final int btn_yearview=0x7f080000;
+        public static final int button1=0x7f080009;
+        public static final int button2=0x7f08000a;
+        public static final int button3=0x7f08000b;
+        public static final int button4=0x7f08000e;
+        public static final int button5=0x7f08000f;
+        public static final int button6=0x7f08000c;
+        public static final int button7=0x7f08000d;
+        public static final int button8=0x7f080010;
+        public static final int button9=0x7f080011;
+        public static final int chk_remember=0x7f080017;
+        public static final int et_password=0x7f080015;
+        public static final int et_username=0x7f080014;
+        public static final int imgv=0x7f080013;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_home=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Crm-Mobile/src/com/Atieh/crm_mobile/MainActivity.java
+++ Crm-Mobile/src/com/Atieh/crm_mobile/MainActivity.java
@@ -1,36 +1,50 @@
 package com.Atieh.crm_mobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
 
+	EditText et_user;
+	EditText et_pass;
+	Button btn_login;
+	CheckBox chk_remember;
+	
+	public void initview(){
+		et_user=(EditText) findViewById(R.id.et_username);
+		et_pass=(EditText) findViewById(R.id.et_password);
+		btn_login=(Button) findViewById(R.id.btn_login);
+		chk_remember=(CheckBox) findViewById(R.id.chk_remember);
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+        
+        btn_login.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				Intent login=new Intent();
+				login.setClass(getApplicationContext(), HomeActivity.class);
+				startActivity(login);
+			}
+		});
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+ 
+   
 }
